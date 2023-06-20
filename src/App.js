a7cda8f2659d84678828e846b84fbcb731a6a2e5
Rashonda Edwards <rashondaedwards@aol.com>
2023-06-19 21:01:14 -0700

    css-practice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-
 import './App.css';
+import { useState } from 'react';
+import { Axios } from 'axios';
 
 function App() {
+
+  const [pokemonName, setPokemonName] = useState('')
+
+  const searchPokemon = ()=>{}
+
   return (
     <div className="App">
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className="header">Pokemon API
+        <div className="searchbar">
+          <input className="input" type="text" onChange={(event)=>{setPokemonName(event.target.value)}} placeholder="Search" />
+          <button className="button" onClick={searchPokemon}>
+            <span>I</span>
+            <span>Choose</span>
+            <span>You!</span>
+          </button>
+        </div>
+      </div>
+      <div className="body">Pokemon</div>
+      <div className="footer">Pokemon Links</div>
     </div>
   );
 }
